f801ae01529bcd958c140a2b5f7831465970e255
Brent Stewart <brent@alienarc.com>
2015-06-21 21:35:57 -0500

    Fixed misspelling

--- Zombiepedia/ZombiepediaService/ZombiepediaService/Models/ZombieInfo.cs
+++ Zombiepedia/ZombiepediaService/ZombiepediaService/Models/ZombieInfo.cs
@@ -11,6 +11,6 @@ namespace ZombiepediaService.Models
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public string Decsription { get; set; }
+		public string Description { get; set; }
 	}
 }
--- Zombiepedia/ZombiepediaService/ZombiepediaService/Services/ZombieDataService.cs
+++ Zombiepedia/ZombiepediaService/ZombiepediaService/Services/ZombieDataService.cs
@@ -15,29 +15,29 @@ namespace ZombiepediaService.Services
             {
                 Id = 1,
                 Name = "Crawler",
-                Decsription =
+                Description =
                     "Unable to walk, either due to missing legs or damaged nerves, and must therefore crawl everywhere. Beware of these and never assume a motionless zombie on the floor is dead."
             },
-            new ZombieInfo() {Name = "Walker", Decsription = "Your standard slow moving zombie. Moves in a jerky shuffle."},
+            new ZombieInfo() {Name = "Walker", Description = "Your standard slow moving zombie. Moves in a jerky shuffle."},
             new ZombieInfo()
             {
                 Id = 2,
                 Name = "Runner",
-                Decsription =
+                Description =
                     "Fast moving and tireless. If you encounter a runner, do not try to outrun it as it will not fatigue and you will. Fortunately they are not very durable and are easy to kill."
             },
             new ZombieInfo()
             {
                 Id = 3,
                 Name = "Teacher",
-                Decsription =
+                Description =
                     "These zombies used to be school teachers and are the traditional brain hungry zombie. The high brain diet and former life as an educator has left them smarter than your average zombie, beware."
             },
             new ZombieInfo()
             {
                 Id = 4,
                 Name = "Teen",
-                Decsription =
+                Description =
                     "Zombified in the midst of adolecense these zombies are easily distracted by TV, smart phones, and any kind of game. Beware, though, they may immediately lose interest in the distraction at any time as their hunger takes over."
             },
         };
