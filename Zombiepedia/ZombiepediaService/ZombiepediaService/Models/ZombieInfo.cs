6074075cf97c44215e8f09494c51752b6a4c93aa
Brent Stewart <brent@alienarc.com>
2015-06-21 22:04:15 -0500

    Added ImagePath to Zombies

--- Zombiepedia/ZombiepediaService/ZombiepediaService/Models/ZombieInfo.cs
+++ Zombiepedia/ZombiepediaService/ZombiepediaService/Models/ZombieInfo.cs
@@ -12,5 +12,6 @@ namespace ZombiepediaService.Models
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
+	    public string ImagePath { get; set; }
 	}
 }
--- Zombiepedia/ZombiepediaService/ZombiepediaService/Services/ZombieDataService.cs
+++ Zombiepedia/ZombiepediaService/ZombiepediaService/Services/ZombieDataService.cs
@@ -9,38 +9,54 @@ namespace ZombiepediaService.Services
 {
     public static class ZombieDataService
     {
-        private static readonly ZombieInfo[] ZombieData = new ZombieInfo[]
+        private static readonly ZombieInfo[] ZombieData;
+
+        static ZombieDataService()
         {
-            new ZombieInfo()
+            var serverAddress = "http://zombiepedia.azurewebsites.net/api/image/";
+            ZombieData = new ZombieInfo[]
             {
-                Id = 1,
-                Name = "Crawler",
-                Description =
-                    "Unable to walk, either due to missing legs or damaged nerves, and must therefore crawl everywhere. Beware of these and never assume a motionless zombie on the floor is dead."
-            },
-            new ZombieInfo() {Name = "Walker", Description = "Your standard slow moving zombie. Moves in a jerky shuffle."},
-            new ZombieInfo()
-            {
-                Id = 2,
-                Name = "Runner",
-                Description =
-                    "Fast moving and tireless. If you encounter a runner, do not try to outrun it as it will not fatigue and you will. Fortunately they are not very durable and are easy to kill."
-            },
-            new ZombieInfo()
-            {
-                Id = 3,
-                Name = "Teacher",
-                Description =
-                    "These zombies used to be school teachers and are the traditional brain hungry zombie. The high brain diet and former life as an educator has left them smarter than your average zombie, beware."
-            },
-            new ZombieInfo()
-            {
-                Id = 4,
-                Name = "Teen",
-                Description =
-                    "Zombified in the midst of adolecense these zombies are easily distracted by TV, smart phones, and any kind of game. Beware, though, they may immediately lose interest in the distraction at any time as their hunger takes over."
-            },
-        };
+                new ZombieInfo()
+                {
+                    Id = 1,
+                    Name = "Crawler",
+                    Description =
+                        "Unable to walk, either due to missing legs or damaged nerves, and must therefore crawl everywhere. Beware of these and never assume a motionless zombie on the floor is dead.",
+                    ImagePath = $"{serverAddress}1"
+                },
+                new ZombieInfo()
+                {
+                    Id = 2,
+                    Name = "Walker",
+                    Description = "Your standard slow moving zombie. Moves in a jerky shuffle.",
+                    ImagePath = $"{serverAddress}2"
+                },
+                new ZombieInfo()
+                {
+                    Id = 3,
+                    Name = "Runner",
+                    Description =
+                        "Fast moving and tireless. If you encounter a runner, do not try to outrun it as it will not fatigue and you will. Fortunately they are not very durable and are easy to kill.",
+                    ImagePath = $"{serverAddress}3"
+                },
+                new ZombieInfo()
+                {
+                    Id = 4,
+                    Name = "Teacher",
+                    Description =
+                        "These zombies used to be school teachers and are the traditional brain hungry zombie. The high brain diet and former life as an educator has left them smarter than your average zombie, beware.",
+                    ImagePath = $"{serverAddress}4"
+                },
+                new ZombieInfo()
+                {
+                    Id = 5,
+                    Name = "Teen",
+                    Description =
+                        "Zombified in the midst of adolecense these zombies are easily distracted by TV, smart phones, and any kind of game. Beware, though, they may immediately lose interest in the distraction at any time as their hunger takes over.",
+                    ImagePath = $"{serverAddress}5"
+                },
+            };
+        }
 
         public static ZombieInfo[] GetZombies()
         {
