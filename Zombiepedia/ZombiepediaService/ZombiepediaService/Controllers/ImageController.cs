c3fc757335a16538332a29889bb203f3a8f87bed
Brent Stewart <brent@alienarc.com>
2015-06-21 19:29:06 -0500

    Added ImageController and one image

---
+++ Zombiepedia/ZombiepediaService/ZombiepediaService/Controllers/ImageController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Hosting;
+using System.Web.Http;
+using ZombiepediaService.Services;
+
+namespace ZombiepediaService.Controllers
+{
+    public class ImageController : ApiController
+    {
+        public HttpResponseMessage GetImage(int id)
+        {
+            var zombieData = ZombieDataService.GetZombies();
+            var zombie = zombieData.FirstOrDefault(z => z.Id == id);
+            if (zombie == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+
+            var filePath = HostingEnvironment.MapPath($"~/Images/{zombie.Id}.jpg");
+            if (!File.Exists(filePath))
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+
+            var memoryStream = new MemoryStream();
+            using (var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                var image = Image.FromStream(fileStream);
+                image.Save(memoryStream, ImageFormat.Jpeg);
+            }
+
+            var result = new HttpResponseMessage(HttpStatusCode.OK);
+            result.Content = new ByteArrayContent(memoryStream.ToArray());
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+
+            return result;
+        }
+    }
+}
---
+++ Zombiepedia/ZombiepediaService/ZombiepediaService/Services/ZombieDataService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZombiepediaService.Models;
+
+namespace ZombiepediaService.Services
+{
+    public static class ZombieDataService
+    {
+        private static readonly ZombieInfo[] ZombieData = new ZombieInfo[]
+        {
+            new ZombieInfo()
+            {
+                Id = 1,
+                Name = "Crawler",
+                Decsription =
+                    "Unable to walk, either due to missing legs or damaged nerves, and must therefore crawl everywhere. Beware of these and never assume a motionless zombie on the floor is dead."
+            },
+            new ZombieInfo() {Name = "Walker", Decsription = "Your standard slow moving zombie. Moves in a jerky shuffle."},
+            new ZombieInfo()
+            {
+                Id = 2,
+                Name = "Runner",
+                Decsription =
+                    "Fast moving and tireless. If you encounter a runner, do not try to outrun it as it will not fatigue and you will. Fortunately they are not very durable and are easy to kill."
+            },
+            new ZombieInfo()
+            {
+                Id = 3,
+                Name = "Teacher",
+                Decsription =
+                    "These zombies used to be school teachers and are the traditional brain hungry zombie. The high brain diet and former life as an educator has left them smarter than your average zombie, beware."
+            },
+            new ZombieInfo()
+            {
+                Id = 4,
+                Name = "Teen",
+                Decsription =
+                    "Zombified in the midst of adolecense these zombies are easily distracted by TV, smart phones, and any kind of game. Beware, though, they may immediately lose interest in the distraction at any time as their hunger takes over."
+            },
+        };
+
+        public static ZombieInfo[] GetZombies()
+        {
+            return ZombieData;
+        }
+    }
+}
